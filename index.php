c6b81d848c97751de80ef235d7ad99d83de3b2c9
YukiTETSUKA <yuki@tedzukayuuki-no-MacBook-Air.local>
2014-01-21 20:30:43 +0900

    index修正

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
 <?php
-	// simple_html_dom.phpをインクルード
-	include_once('simplehtmldom_1_5/simple_html_dom.php');
-	$filename = "count.txt";
+// simple_html_dom.phpをインクルード
+include_once('simplehtmldom_1_5/simple_html_dom.php');
+$filename = "count.txt";
 
-	// スクレイピングしたいURLを指定
-	$html = file_get_html( 'http://blogs.yahoo.co.jp/minamihata55/35579205.html' );
-	//echo $html;
-	
-	$people = $html->find('div.moduleTableArea table tbody tr td');
+// スクレイピングしたいURLを指定
+$html = file_get_html( 'http://blogs.yahoo.co.jp/minamihata55/35579205.html' );
+//echo $html;
 
-	$date = date('Y / m / d ');
-	file_put_contents($filename, $date.' ' , FILE_APPEND);
+$people = $html->find('div.moduleTableArea table tbody tr td');
 
-	foreach ($people as $key => $varlue) {
-		if ($key >= 8) {
-			break;
-		}else if($key%2 == 0){
-			echo($varlue);
-			file_put_contents($filename, $varlue->plaintext.' ' , FILE_APPEND);
+$date = date('Ymd');
+file_put_contents($filename, $date."\t" , FILE_APPEND);
 
-		}
+foreach ($people as $key => $varlue){
+    if ($key >= 8){
+        break;
+    }else if($key%2 == 0){
+        echo($varlue);
+        file_put_contents($filename, $varlue->plaintext."\t" , FILE_APPEND);
+    }
+}
 
-	}
-	file_put_contents($filename,"\n" , FILE_APPEND);
-	
-	$html->clear();
+foreach ($people as $key => $varlue){
+    if ($key >= 8){
+        break;
+    }else if($key%2 != 0){
+        echo($varlue);
+        file_put_contents($filename, $varlue->plaintext."\t" , FILE_APPEND);
+    }	
+}
+
+file_put_contents($filename,"\n" , FILE_APPEND);
+
+$html->clear();
 ?>
